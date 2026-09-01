d9fb07d8ce6321fc4e9b46eb111d89ec1e9bd1a4
ismoil1jr <isroile70@gmail.com>
2026-09-01 12:40:38 +0500

    Fix: bot logs, check static folder

--- start.sh
+++ start.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e
 
-# Botni background da ishga tushirish
-python bot.py &
+# Botni background da ishga tushirish va logga yozish
+python -u bot.py > bot.log 2>&1 &
 
-# Flask serverni ishga tushirish (gunicorn)
+# Flask serverni ishga tushirish
 gunicorn main:app -b 0.0.0.0:8000
